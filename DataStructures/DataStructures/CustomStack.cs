b73138bcd049f16b3e8bc90907ce5eef14448ddd
Eric Muthomi <emuthomi@jambopay.com>
2025-04-02 21:54:45 +0300

    Custom stack implementation

---
+++ DataStructures/DataStructures/CustomStack.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+namespace DataStructures
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private Node? _top;
+
+        public void Push(T data)
+        {
+            var newNode = new Node(data) { Next = _top };
+            _top = newNode;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+
+            var data = _top!.Data;
+            _top = _top.Next;
+            return data;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+            return _top!.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return _top == null;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = _top;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class Node
+        {
+            public T Data { get; set; }
+            public Node? Next { get; set; }
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+    }
+}
