39922b46f95989d90d9276c4e9bf972019b25417
Eric Muthomi <emuthomi@jambopay.com>
2025-04-02 20:09:14 +0300

    Singly linked list

---
+++ DataStructures/DataStructures/Singly/SinglyLinkedList.cs
@@ -0,0 +1,101 @@
+﻿namespace DataStructures.Singly
+{
+    internal class SinglyLinkedList<T>
+    {
+        private int _size;
+        private Node<T>? _head = default;
+        private Node<T>? _tail = default;
+
+        public void InsertAtEnd(T data)
+        {
+            var newNode = new Node<T>(data);
+            _size++;
+            if (IsEmpty())
+            {
+                _head = _tail = newNode;
+                return;
+            }
+            _tail!.Next = newNode;
+        }
+
+        public void InsertAtBeginning(T data)
+        {
+            Node<T> newNode = new Node<T>(data) { Next = _head };
+
+            // Move the head to the new node
+            _head = newNode;
+            // Check whether the this was the first insert
+            if (_tail == null)
+                _tail = _head;
+            _size++;
+        }
+
+        // Assumes there is no duplicate
+        public bool Delete(T data)
+        {
+            if (IsEmpty()) return false;
+            if (_head!.Data!.Equals(data))
+            {
+                _head = _head.Next;
+                if (_head == null) _tail = null;
+                _size--;
+                return true;
+            }
+            Node<T> temp = _head;
+            while (temp.Next != null && !temp.Next.Data!.Equals(data))
+            {
+                temp = temp.Next;
+            }
+            if (temp.Next == null) return false;
+            if (temp.Next == _tail)
+            {
+                _tail = temp;
+            }
+            temp.Next = temp.Next.Next;
+            _size--;
+            return true;
+        }
+
+        public bool Search(T data)
+        {
+            Node<T>? temp = _head;
+            while (temp != null)
+            {
+                if (temp.Data!.Equals(data)) return true;
+                temp = temp.Next;
+            }
+            return false;
+        }
+
+        public void PrintList()
+        {
+            Node<T>? temp = _head;
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " -> ");
+                temp = temp.Next;
+            }
+            Console.WriteLine("null");
+        }
+        public int size()
+        {
+            return _size;
+        }
+
+        // Is this linked list empty?
+        public bool IsEmpty()
+        {
+            return size() == 0;
+        }
+        private class Node<Type>
+        {
+            public Node(Type data)
+            {
+                Data = data;
+                Next = null;
+            }
+            public Type Data { get; set; }
+            public Node<Type>? Next { get; set; }
+        }
+    }
+}
